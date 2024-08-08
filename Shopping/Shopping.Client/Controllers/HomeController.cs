77c72de544cbcdd1d10ce2ea437aef6c468d53a1
TheSatishPawar <er.satishpawar@gmail.com>
2024-08-08 14:50:58 +0530

    init commit

---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shopping.Client.Data;
+using Shopping.Client.Models;
+using System.Diagnostics;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory  httpClientFactory)
+        {
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("/Product");
+            var content = await response.Content.ReadAsStringAsync();
+            var productList= JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+
+            return View(productList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
